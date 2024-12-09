8bcc28ecfae07253f46ab6e3789b5eb6fd0e24af
Marikita1007 <ishinomaki1007@gmail.com>
2024-12-09 20:39:13 +0100

    Laravel_15 : Finished Understanding Database Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Job;
 use Illuminate\Database\Seeder;
+use function Laravel\Prompts\password;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'first_name' => 'John',
+             'last_name' => 'Doe',
+             'email' => 'test@example.com',
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         $this->call(JobSeeder::class);
+
     }
 }
